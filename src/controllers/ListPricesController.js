a01907cd61a8a0a3aab58303b5f7ac1fd45b3b41
Guilherme <guilherme.s.goncalves@outlook.com>
2022-09-28 18:50:05 -0300

    list price per beneficiary

---
+++ src/controllers/ListPricesController.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const { platform } = require('os');
+const path = require('path');
+
+module.exports = class ListPricesController {
+    static listPrice(req, res) {
+        const prices = JSON.parse(
+            fs.readFileSync(path.resolve(__dirname, '../data/prices.json'), {
+                encoding: 'utf-8',
+            })
+        );
+
+        const plans = JSON.parse(
+            fs.readFileSync(path.resolve(__dirname, '../data/plans.json'), {
+                encoding: 'utf-8',
+            })
+        );
+
+        const beneficiaries = JSON.parse(
+            fs.readFileSync(
+                path.resolve(__dirname, '../data/beneficiarios.json'),
+                {
+                    encoding: 'utf-8',
+                }
+            )
+        );
+
+        const register = beneficiaries.find((val) => val);
+        const plan = plans.find((val) => val.registro === register.registro);
+
+        const price = prices.find((val) => val.codigo === plan.codigo);
+
+        const listAges = register.beneficiarios;
+
+        const age = listAges.map((listAge) => {
+            return listAge.age;
+        });
+
+        const pricePerBeneficiary = [];
+
+        for (let i in age) {
+            if (age[i] <= 17) {
+                pricePerBeneficiary.push({
+                    idade: age[i],
+                    preço: price.faixa1,
+                });
+            } else if (age[i] >= 18 && age[i] <= 40) {
+                pricePerBeneficiary.push({
+                    idade: age[i],
+                    preço: price.faixa2,
+                });
+            } else {
+                pricePerBeneficiary.push({
+                    idade: age[i],
+                    preço: price.faixa3,
+                });
+            }
+        }
+
+        res.status(200).json({ pricePerBeneficiary });
+    }
+};
--- src/routes/pricesRoute.js
+++ src/routes/pricesRoute.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const GetPriceController = require('../controllers/GetPriceController');
+const ListPricesController = require('../controllers/ListPricesController');
 
 router.get('/', GetPriceController.getPrices);
+router.get('/list', ListPricesController.listPrice);
 
 module.exports = router;
